9e427483a475079e60fba81c143c657378a7e407
postgovnar <109201132+postgovnar@users.noreply.github.com>
2025-03-13 14:53:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bot import bot_main
 context = {
-    'token': '6749400221:AAGdCxorRoZSqnI0eJiUcuhlQXb_39cEF14',
+    'token': 'API_TOKEN',
     'chanel_id': '-1002023642883',
     'log_chat_id': '-1002026292785',
     'pay_token': "381764678:TEST:83859",
